4d274d70d238b6332f38748f8a6e4124d299d7cf
SyntaxThaw <weven-xenon0n@icloud.com>
2026-04-27 15:44:18 +0200

    Fix Matter re-registration for cached accessories

--- src/platform.ts
+++ src/platform.ts
@@ -228,19 +228,21 @@ export class EufyRobovacMatterPlatform implements DynamicPlatformPlugin {
       statePushSupported = false;
     }
 
-    if (isNewAccessory) {
-      if (matterApi?.registerPlatformAccessories) {
-        await matterApi.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    if (matterApi?.registerPlatformAccessories) {
+      await matterApi.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      if (isNewAccessory) {
+        this.accessories.push(accessory);
+        this.log.info(`Registered Matter accessory: ${accessory.displayName}`);
       } else {
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        this.log.debug(`Re-registered cached Matter accessory for current session: ${accessory.displayName}`);
       }
-      this.accessories.push(accessory);
-      this.log.info(`Registered Matter accessory: ${accessory.displayName}`);
       return { configured: true, statePushSupported };
     }
 
-    if (matterApi?.updatePlatformAccessories) {
-      await matterApi.updatePlatformAccessories([accessory]);
+    if (isNewAccessory) {
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      this.accessories.push(accessory);
+      this.log.info(`Registered Homebridge accessory: ${accessory.displayName}`);
     } else {
       this.api.updatePlatformAccessories([accessory]);
     }
