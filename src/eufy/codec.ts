cc380729e2ec83582aa6ade3f8ba46665291b534
SyntaxThaw <weven-xenon0n@icloud.com>
2026-04-28 10:47:11 +0200

    Fix Matter start command protobuf type loading

--- src/eufy/codec.ts
+++ src/eufy/codec.ts
@@ -26,7 +26,9 @@ export class EufyCodec {
     await this.root.load([
       path.join(protoDir, 'work_status.proto'),
       path.join(protoDir, 'clean_param.proto'),
-      path.join(protoDir, 'error_code.proto')
+      path.join(protoDir, 'error_code.proto'),
+      path.join(protoDir, 'control.proto'),
+      path.join(protoDir, 'station.proto')
     ]);
   }
 
